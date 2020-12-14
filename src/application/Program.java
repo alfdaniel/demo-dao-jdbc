d98f6ff5288cc8e39a886a51edc173bcc6957657
alfdaniel <daniel.alexandre22@hotmail.com>
2020-12-14 20:45:40 -0300

    Departament entity class

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Departament;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Departament obj = new Departament(42, "Daniel");
+		
+		System.out.println(obj);
+	}
+
+}
