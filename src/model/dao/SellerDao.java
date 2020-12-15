23ab05db9422f4e812fd9388e26e3f33ec5e21db
alfdaniel <daniel.alexandre22@hotmail.com>
2020-12-15 00:01:04 -0300

    DepartmentDao e SellerDao interfaces

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -5,11 +5,11 @@ import java.util.List;
 import model.entities.Seller;
 
 public interface SellerDao {
-
 	
 	void insert(Seller obj);
 	void update(Seller obj);
 	void delete(Seller obj);
 	Seller findById(Integer id);
 	List<Seller> findAll();
+	
 }
