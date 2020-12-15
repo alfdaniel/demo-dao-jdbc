095116f723902ee475c0c200d74753595abd53a7
alfdaniel <daniel.alexandre22@hotmail.com>
2020-12-14 23:13:04 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,19 +2,19 @@ package application;
 
 import java.util.Date;
 
-import model.entities.Departament;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Departament obj = new Departament(42, "Daniel");
-		Seller seller = new Seller(01, "Daniel", "@gmail", new Date(), 3000.0, obj);
+		Department department = new Department(42, "Books");
+		Seller seller = new Seller(01, "Daniel", "@gmail", new Date(), 3000.0, department);
 		
 		System.out.println(seller);
+		System.out.println(department);
 		
-		System.out.println(obj);
 	}
 
 }
